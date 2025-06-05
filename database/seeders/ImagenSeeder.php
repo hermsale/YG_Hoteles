96db31cd51485182d826a1141fd696b5068882ef
hermsale <hermsale@gmail.com>
2025-06-05 20:27:05 -0300

    agregue el img a public para guardar las imagenes

--- database/seeders/ImagenSeeder.php
+++ database/seeders/ImagenSeeder.php
@@ -11,10 +11,10 @@ class ImagenSeeder extends Seeder
     public function run()
     {
          $imagenes = [
-            ['url' => 'https://hotel.com/img/deluxe101.jpg'],
-            ['url' => 'https://hotel.com/img/simple201.jpg'],
-            ['url' => 'https://hotel.com/img/doble301.jpg'],
-            ['url' => 'https://hotel.com/img/triple401.jpg'],
+            ['url' => './img/habitacion-deluxe101-1.jpg'],
+            ['url' => './img/habitacion-simple201-1.jpg'],
+            ['url' => 'img/habitacion-doble301-1.jpg'],
+            ['url' => 'img/habitacion-triple401-1.jpg'],
         ];
 
         foreach ($imagenes as $imagen) {
