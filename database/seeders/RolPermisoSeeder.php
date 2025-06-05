cfcacbe60f7fa99cf9b7a636a1ac2e45b4f8d4c9
AleDaVinci <alejandro.herms@davinci.edu.ar>
2025-06-05 16:02:56 -0300

    se actualizo el RolPermiso

--- database/seeders/RolPermisoSeeder.php
+++ database/seeders/RolPermisoSeeder.php
@@ -14,16 +14,42 @@ class RolPermisoSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $admin = Rol::where('nombre_rol', 'Administrador')->first();
         $recepcionista = Rol::where('nombre_rol', 'Recepcionista')->first();
         $cliente = Rol::where('nombre_rol', 'Cliente')->first();
 
-        // IDs o nombres de los permisos
-        $permisosAdmin = ['crear cursos', 'leer cursos', 'modificar cursos', 'eliminar cursos'];
-        $permisosCliente = ['leer cursos'];
+        // permisos para recepcionista
+        $permisosRecepcionista = [
+            'crear reserva',
+            'editar reserva',
+            'cancelar reserva',
+            'ver reserva',
+            // 'crear habitacion',
+            // 'editar habitacion',
+            'ver habitacion',
+            'ver usuario',
+            // 'ver reportes',
+            'abrir caja',
+            'cerrar caja',
+            'ver caja',
+            // 'realizar arqueo de caja',
+        ];
 
-        // Sync permite asociar sin duplicar
+        // permisos para cliente
+        $permisosCliente = [
+            'crear reserva',
+            'ver reserva',
+        ];
+
+        // permisos para administrador
+        $permisosAdmin = Permiso::pluck('id')->toArray(); // todos
+
+        // Asignación
+        $admin->permisos()->sync($permisosAdmin);
+
         $recepcionista->permisos()->sync(
-            Permiso::whereIn('nombre_permiso', $permisosAdmin)->pluck('id')->toArray()
+            Permiso::whereIn('nombre_permiso', $permisosRecepcionista)->pluck('id')->toArray()
         );
 
         $cliente->permisos()->sync(
