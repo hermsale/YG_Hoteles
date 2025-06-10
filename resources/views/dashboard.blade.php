7b833e4463abee1076ce4bd1b1d45ba717972223
AleDaVinci <alejandro.herms@davinci.edu.ar>
2025-06-10 08:42:38 -0300

    se elimino archivos dashboardHotel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-   
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -15,7 +15,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h1 class="text-2xl font-bold mb-4">Imagenes Habitaciones</h1>
-
                 </div>
             </div>
         </div>
